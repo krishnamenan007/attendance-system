cb5b3320bcd78520bc694f9f39fa8d014e06695a
krishnamenan007 <krishnamenan007@gmail.com>
2025-01-29 12:47:12 +0530

    #2.1,LoginController code completed with request, response models

--- server/src/main/java/com/attendance_system/controller/LoginController.java
+++ server/src/main/java/com/attendance_system/controller/LoginController.java
@@ -1,6 +1,11 @@
 package com.attendance_system.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.attendance_system.model.LoginRequest;
+import com.attendance_system.model.LoginResponse;
+import com.attendance_system.service.AuthService;
 
 
 @RestController
@@ -9,6 +14,17 @@ import org.springframework.web.bind.annotation.*;
 
 
 public class LoginController {
+    @Autowired
+    private AuthService authService;
 
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        String token = authService.authenticateUser(loginRequest.getUsername(), loginRequest.getPassword());
+        return ResponseEntity.ok(new LoginResponse(token));
+    }
+    @GetMapping("/check")
+    public String checkController() {
+        return "Controller is working!";
+    }
    
 }
