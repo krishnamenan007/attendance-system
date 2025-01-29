6761ab70214b09f1b761b1c32b0f5d04b14f5147
krishnamenan007 <krishnamenan007@gmail.com>
2025-01-29 12:35:29 +0530

    #2, controller created to handle user authentication

---
+++ server/src/main/java/com/attendance_system/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.attendance_system.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")  // Allows any origin for API testing purpose
+
+
+public class LoginController {
+
+   
+}
