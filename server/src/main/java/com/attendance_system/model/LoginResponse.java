3f9a4d64d8f1cfb13a9c87533cd8a9080e02be0f
krishnamenan007 <krishnamenan007@gmail.com>
2025-01-29 12:40:11 +0530

    #4, model created send response to client

---
+++ server/src/main/java/com/attendance_system/model/LoginResponse.java
@@ -0,0 +1,17 @@
+package com.attendance_system.model;
+
+public class LoginResponse {
+    private String token;
+
+    public LoginResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
